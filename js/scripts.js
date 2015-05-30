3f76a47fa7c2a61864063497b457f8065a6480d2
Sierra Morgan <sierramorgan07@gmail.com>
2015-05-29 18:35:56 -0700

    added external js

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+	$('#b1').click(function() {
+		$('#hrwkd1').show();
+		$('#tmtkn1').show();
+		$('#cldby1').show();
+		$('#hrwkd2').hide();
+		$('#tmtkn2').hide();
+		$('#cldby2').hide();
+		$('#hrwkd3').hide();
+		$('#tmtkn3').hide();
+		$('#cldby3').hide();
+		$('#hrwkd4').hide();
+		$('#tmtkn4').hide();
+		$('#cldby4').hide();
+		$('#hrwkd5').hide();
+		$('#tmtkn5').hide();
+		$('#cldby5').hide();
+	});
+
+	$('#b2').click(function() {
+		$('#hrwkd1').hide();
+		$('#tmtkn1').hide();
+		$('#cldby1').hide();
+		$('#hrwkd2').show();
+		$('#tmtkn2').show();
+		$('#cldby2').show();
+		$('#hrwkd3').hide();
+		$('#tmtkn3').hide();
+		$('#cldby3').hide();
+		$('#hrwkd4').hide();
+		$('#tmtkn4').hide();
+		$('#cldby4').hide();
+		$('#hrwkd5').hide();
+		$('#tmtkn5').hide();
+		$('#cldby5').hide();
+	});
+
+	$('#b3').click(function() {
+		$('#hrwkd1').hide();
+		$('#tmtkn1').hide();
+		$('#cldby1').hide();
+		$('#hrwkd2').hide();
+		$('#tmtkn2').hide();
+		$('#cldby2').hide();
+		$('#hrwkd3').show();
+		$('#tmtkn3').show();
+		$('#cldby3').show();
+		$('#hrwkd4').hide();
+		$('#tmtkn4').hide();
+		$('#cldby4').hide();
+		$('#hrwkd5').hide();
+		$('#tmtkn5').hide();
+		$('#cldby5').hide();
+	});
+
+	$('#b4').click(function() {
+		$('#hrwkd1').hide();
+		$('#tmtkn1').hide();
+		$('#cldby1').hide();
+		$('#hrwkd2').hide();
+		$('#tmtkn2').hide();
+		$('#cldby2').hide();
+		$('#hrwkd3').hide();
+		$('#tmtkn3').hide();
+		$('#cldby3').hide();
+		$('#hrwkd4').show();
+		$('#tmtkn4').show();
+		$('#cldby4').show();
+		$('#hrwkd5').hide();
+		$('#tmtkn5').hide();
+		$('#cldby5').hide();
+	});
+
+	$('#b5').click(function() {
+		$('#hrwkd1').hide();
+		$('#tmtkn1').hide();
+		$('#cldby1').hide();
+		$('#hrwkd2').hide();
+		$('#tmtkn2').hide();
+		$('#cldby2').hide();
+		$('#hrwkd3').hide();
+		$('#tmtkn3').hide();
+		$('#cldby3').hide();
+		$('#hrwkd4').hide();
+		$('#tmtkn4').hide();
+		$('#cldby4').hide();
+		$('#hrwkd5').show();
+		$('#tmtkn5').show();
+		$('#cldby5').show();
+	});
+
+});
